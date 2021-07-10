1207a857bc902776ac5309b2c8506a1541d5835e
Rohan Jebaraj <rohanjebaraj.23it@licet.ac.in>
2021-07-10 22:58:16 +0530

    react

--- src/pages/components/home/CreateNote.jsx
+++ src/pages/components/home/CreateNote.jsx
@@ -1,6 +1,7 @@
+
 import { useState } from "react";
 import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
+import Textfield from "@material-ui/core/TextField";
 import Snackbar from "@material-ui/core/Snackbar";
 import MuiAlert from "@material-ui/lab/Alert";
 import firebase from "firebase/app";
@@ -11,85 +12,88 @@ export default function CreateNote() {
     function Alert(props) {
         return <MuiAlert elevation={6} variant="filled" {...props} />;
     }
-
-    const [open, setOpen] = useState(false);
+    const [ open, setOpen] = useState(false);
 
     const handleClose = () => {
         setOpen(false);
     };
-
-    const [state, setState] = useState({
-        note: "",
-    });
-
-    const handleChange = (e) => {
-        const { id, value } = e.target;
-        setState((prevState) => ({
-            ...prevState,
-            [id]: value,
-
+    const showFile = async (e) => {
+        e.preventDefault()
+        const reader = new FileReader()
+        reader.onload = async (e) => { 
+        const text = (e.target.result)
+        console.log(text)
+       // alert(text)
+        setState(() => ({
+            note: text,
         }));
-
-    };
+        };
+        reader.readAsText(e.target.files[0])
+    }
+        const [state, setState] = useState({
+            note: "",
+        });
+        const handleChange = (e) => {
+            const { id, value } = e.target;
+            setState((prevState) => ({
+                ...prevState,
+                [id]: value,
+            }));
+        };    
 
     const handleSubmitClick = (e) => {
         e.preventDefault();
         console.log(state.note);
-        const note = state.note;
+        const note =state.note;
         var db = firebase.firestore();
         if(state.note != "") {
             db.collection("notes")
         .add({
-          note: note,
-          created: firebase.firestore.FieldValue.serverTimestamp(),
+            note: note,
+            created: firebase.firestore.FieldValue.serverTimestamp(),
         })
         .then((docRef) => {
-          console.log("Document written with ID: ", docRef.id);
-          setOpen(true);
-          setState((prevState) => ({
-              ...prevState,
-              note: "",
-          }));
+            console.log("Document written with ID: ", docRef.id);
+            setOpen(true);
+            setState((prevState) => ({
+                ...prevState,
+                note: "",
+            }));
         })
         .catch((error) => {
-          console.error("Error adding document: ", error);
+            console.error("Error adding document: ", error);
         });
         }
     };
 
-
     return(
         <>
-        <div className="container col-md-8 col-xl-6 mt-4">
-            <form action="#">
-                <TextField id="note"
-                 label="Create a new note..."
-                variant="filled"
-                rows={4} 
-                className="shadow-sm"
-                fullWidth
-                multiline
-                onChange={handleChange}
-                value={state.note}
-                />
-                <Button 
-                variant="contained" 
-                color="primary" 
-                className="d-flex ml-auto mt-3 text-capitalize font-weight-bold"
-             onClick={handleSubmitClick}
-                >Create
-
-                </Button>
-            </form>
-
-        </div>
-        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-            <Alert onClose={handleClose} severity="success">
-                Note created successfully!
-            </Alert>
-        </Snackbar>
-
-
+            <div className="container mt-4">
+                <form action="#">
+                    <Textfield id="note" label="Create a new note..."
+                    variant="outlined" color="primary"rows={4} 
+                    className="shadow-sm" 
+                    fullWidth
+                    multiline
+                    onChange={handleChange}
+                    value={state.note}
+                    />
+                    <input type="file" className="btn btn-primary"  accept=".txt" onChange={(e) => showFile(e)} />
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        className="d-flex ml-auto mt-3 text-capitalize font-weight-bold"
+                        onClick={handleSubmitClick}
+                        >
+                            Create
+                    </Button>
+                </form>
+            </div>
+            <Snackbar open = {open} autoHideDuration={6000} onClose={handleClose}>
+                <Alert onClose={handleClose} severity="success">
+                    Note Created Successfully
+                </Alert>
+            </Snackbar>
         </>
-    )
+    );
 }
